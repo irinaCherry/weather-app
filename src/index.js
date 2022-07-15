cb9a97f0adcd44988a4291aef7fb2d3c002966bb
irinaCherry <cherneha.i@gmail.com>
2022-07-15 22:47:53 +0200

    updateWeatherIcon function eas added, other functions renamed

--- src/index.js
+++ src/index.js
@@ -29,8 +29,53 @@ function getCurrentDay(fullDate) {
   return `${days[dayOfWeek]}, ${months[month]} ${day}, ${localtime}`;
 }
 
-function getTemperature(response) {
-  console.log(response);
+function updateWeatherIcon(iconId) {
+  let weatherIconElement = document.querySelector("#weather-icon");
+  if (iconId === "01n") {
+    weatherIconElement.classList.remove(weatherIconElement.classList.item(2));
+    weatherIconElement.classList.add("fa-moon");
+  } else if (iconId === "01d") {
+    weatherIconElement.classList.remove(weatherIconElement.classList.item(2));
+    weatherIconElement.classList.add("fa-sun");
+  } else if (iconId === "02d") {
+    weatherIconElement.classList.remove(weatherIconElement.classList.item(2));
+    weatherIconElement.classList.add("fa-cloud-sun");
+  } else if (iconId === "02n") {
+    weatherIconElement.classList.remove(weatherIconElement.classList.item(2));
+    weatherIconElement.classList.add("fa-cloud-moon");
+  } else if (
+    (iconId === "03d") |
+    (iconId === "03n") |
+    (iconId === "04d") |
+    (iconId === "04n")
+  ) {
+    weatherIconElement.classList.remove(weatherIconElement.classList.item(2));
+    weatherIconElement.classList.add("fa-cloud");
+  } else if ((iconId === "09d") | (iconId === "09n")) {
+    weatherIconElement.classList.remove(weatherIconElement.classList.item(2));
+    weatherIconElement.classList.add("fa-cloud-showers-heavy");
+  } else if (iconId === "10d") {
+    weatherIconElement.classList.remove(weatherIconElement.classList.item(2));
+    weatherIconElement.classList.add("fa-cloud-sun-rain");
+  } else if (iconId === "10n") {
+    weatherIconElement.classList.remove(weatherIconElement.classList.item(2));
+    weatherIconElement.classList.add("fa-cloud-moon-rain");
+  } else if ((iconId === "11d") | (iconId === "11n")) {
+    weatherIconElement.classList.remove(weatherIconElement.classList.item(2));
+    weatherIconElement.classList.add("fa-cloud-bolt");
+  } else if ((iconId === "13d") | (iconId === "13n")) {
+    weatherIconElement.classList.remove(weatherIconElement.classList.item(2));
+    weatherIconElement.classList.add("fa-snowflake");
+  } else if ((iconId === "50d") | (iconId === "50n")) {
+    weatherIconElement.classList.remove(weatherIconElement.classList.item(2));
+    weatherIconElement.classList.add("fa-smog");
+  } else {
+    weatherIconElement.classList.remove(weatherIconElement.classList.item(2));
+    weatherIconElement.classList.add("fa-hurricane");
+  }
+}
+
+function updateCurrentTemperature(response) {
   let temperature = Math.round(response.data.main.temp);
   document.querySelector(
     "#current-city"
@@ -40,28 +85,29 @@ function getTemperature(response) {
   ).innerHTML = `${response.data.weather[0].description}`;
   document.querySelector("#search-city").value = "";
   document.querySelector("#degree").innerHTML = temperature;
+  updateWeatherIcon(response.data.weather[0].icon);
 }
 
-function calculateCoordinates(position) {
+function findWeatherByCoordinates(position) {
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
   let apiKey = "409663116819999f86eb04964bec2384";
   let apiUrl = "https://api.openweathermap.org/data/2.5/weather";
   let endPoint = `${apiUrl}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-  axios.get(endPoint).then(getTemperature);
+  axios.get(endPoint).then(updateCurrentTemperature);
 }
 
 function startCalculationCoordinates() {
-  navigator.geolocation.getCurrentPosition(calculateCoordinates);
+  navigator.geolocation.getCurrentPosition(findWeatherByCoordinates);
 }
 
-function showTheWeatherIntheCity(event) {
+function findWeatherByCity(event) {
   event.preventDefault();
   let searchCity = document.querySelector("#search-city");
   let apiKey = "409663116819999f86eb04964bec2384";
   let apiUrl = "https://api.openweathermap.org/data/2.5/weather";
   let endPoint = `${apiUrl}?q=${searchCity.value}&appid=${apiKey}&units=metric`;
-  axios.get(endPoint).then(getTemperature);
+  axios.get(endPoint).then(updateCurrentTemperature);
 }
 
 function updateTemperatureToFahrenheit() {
@@ -81,7 +127,7 @@ let currentDateElement = document.querySelector("#current-date");
 currentDateElement.innerHTML = getCurrentDay(currentDate);
 
 let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", showTheWeatherIntheCity);
+searchForm.addEventListener("submit", findWeatherByCity);
 
 let degree = document.querySelector("#degree");
 let celsiusUnits = document.querySelector("#celsius-units");
